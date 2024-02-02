4e2decac6400d77e07046bf24e0260e070f13e99
1KingofClayland <claylandlee@gmail.com>
2024-02-01 16:44:46 -0800

    changed ArrayExamples methods and added tests to ArrayTests

--- ArrayTests.java
+++ ArrayTests.java
@@ -4,9 +4,17 @@ import org.junit.*;
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+    int[] input1 = {3};
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
+
+    int[] input2 = {1,2,3};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{3,2,1}, input2);
+
+    int[] input3 = {1,2,3,4,5,6};
+    ArrayExamples.reverseInPlace(input3);
+    assertArrayEquals(new int[]{6,5,4,3,2,1}, input3);
 	}
 
 
@@ -14,5 +22,14 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+
+    int[] input2 = {3};
+    assertArrayEquals(new int[]{3}, ArrayExamples.reversed(input2));
+
+    int[] input3 = { 1,2,3 };
+    assertArrayEquals(new int[]{3,2,1}, ArrayExamples.reversed(input3));
+
+    int[] input4 = { 1,2,3,4,5,6 };
+    assertArrayEquals(new int[]{6,5,4,3,2,1}, ArrayExamples.reversed(input4));
   }
 }
